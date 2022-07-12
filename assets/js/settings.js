45f5271990ab0f7e825895faa1d6100efccced89
Swati Jha <swatijha.pillarsoft@gmail.com>
2022-07-12 22:10:22 +0530

    rami-changes

---
+++ assets/js/settings.js
@@ -0,0 +1,61 @@
+var ogScreenWidth = $(window).width();
+
+$(document).ready(function () {
+  $("#dashboard-sidebar").on("show.bs.collapse", function () {
+    if ($(window).width() <= 1200) {
+      $("body").addClass("sidebar-active");
+    } else {
+      $("body").removeClass("sidebar-active");
+    }
+    $(this).animate(
+      {
+        width: 220,
+      },
+      600
+    );
+  });
+
+  $("#dashboard-sidebar").on("hide.bs.collapse", function (e) {
+    e.preventDefault();
+    $(this).toggleClass("show");
+    $("body").removeClass("sidebar-active");
+    $(this).animate(
+      {
+        width: 0,
+      },
+      600
+    );
+  });
+
+  if ($(window).width() >= 1200) {
+    $("#dashboard-sidebar").trigger("show.bs.collapse");
+  } else {
+    $("#dashboard-sidebar").trigger("hide.bs.collapse");
+  }
+
+  $(".dashboard-sidebar-overlay").click(function () {
+    $("#dashboard-sidebar").trigger("hide.bs.collapse");
+  });
+});
+
+function sidebarMobileResized() {
+  if ($(window).width() != ogScreenWidth) {
+    console.log($(window).width());
+    if ($(window).width() < 1200) {
+      $("body").addClass("sidebar-active");
+      $("#dashboard-sidebar").trigger("show.bs.collapse");
+    } else {
+      $("body").removeClass("sidebar-active");
+    }
+  }
+}
+
+$(window).resize(sidebarMobileResized);
+
+$(".setting-profile-photo input").change(function (e) {
+  let file = e.target.files[0];
+  if (file) {
+    let newImgUrl = URL.createObjectURL(file);
+    $("#userUploadedImg").attr("src", newImgUrl);
+  }
+});
